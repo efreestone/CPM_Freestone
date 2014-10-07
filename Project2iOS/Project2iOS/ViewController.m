fc8cba52d91defb8cf9636a9f7db87c2db249418
efreestone <efreestone@fullsail.edu>
2014-10-06 20:44:19 -0600

    Added basic login/sign up using Parse tutorial. Both are working correctly but no log out or skinning/overriding Parse style yet

--- Project2iOS/Project2iOS/ViewController.m
+++ Project2iOS/Project2iOS/ViewController.m
@@ -20,13 +20,35 @@
 @implementation ViewController
  
 - (void)viewDidLoad {
-    //Test parse
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];
+//    //Test parse
+//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+//    testObject[@"foo"] = @"bar";
+//    [testObject saveInBackground];
     
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    //Check if a user is signed in, present login view if not.
+    if(![PFUser currentUser]) { 
+        //Instantiate login view controller
+        PFLogInViewController *loginViewController = [[PFLogInViewController alloc] init];
+        [loginViewController setDelegate:self];
+        
+        //Instantiate sign up view controller
+        PFSignUpViewController *signUpViewController = [[PFSignUpViewController alloc] init];
+        [signUpViewController setDelegate:self];
+        
+        //Link sign up to be displayed from login
+        [loginViewController setSignUpController:signUpViewController];
+        
+        //Present login view controller
+        [self presentViewController:loginViewController animated:YES completion:NULL];
+    } else {
+        NSLog(@"User auto-logged in");
+    }
+
 }
 
 - (void)didReceiveMemoryWarning {
