d9db71c34a46c2d23e259c21065425c472aa084f
efreestone <efreestone@fullsail.edu>
2014-10-17 16:02:40 -0600

    Initial commit for project 4 with blank single view activity. Will rebuild project 2 next before moving on to this weeks assignment

---
+++ Project4iOS/Project4iOS/ViewController.m
@@ -0,0 +1,32 @@
+// Elijah Freestone
+// CPM 1410
+// Week 4
+// October 17th, 2014
+
+//
+//  ViewController.m
+//  Project4iOS
+//
+//  Created by Elijah Freestone on 10/17/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
